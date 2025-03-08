51c18773885e4fc7f497430258a29ff8b39adb2b
StivenDelgado <delgadostiven123@gmail.com>
2025-03-08 15:45:36 -0500

    finished serverless and docker db2

--- aws-python-http-api-project/handler.py
+++ aws-python-http-api-project/handler.py
@@ -1,12 +1,31 @@
 import json
 
 
-def hello(event, context):
+def five_atributte(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "id": 1,
+        "name": "Yamaha",
+        "color": "Red",
+        "price": 1000,
+        "description": "Moto de fábrica"
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
+    return response
+
+def ten_atributte(event, context):
+    body = {
+        "id": 1,
+        "name": "Stiven",   
+        "lastname": "Parcial",
+        "age": 25,
+        "city": "Armenia",
+        "country": "Colombia",
+        "email": "stiven.parcial@gmail.com",
+        "phone": "+57 324 281 5979",
+        "address": "Calle 50 #A mnz C #9",
+        "description": "Estudiante de Ingeniería Informática"
+    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
     return response
